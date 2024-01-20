cd59b190e1bcb7f64162fd66c31e6ecb200aeb11
theokarvoun <155096086+theokarvoun@users.noreply.github.com>
2024-01-20 22:59:38 +0200

    Added project and mod folder creation
    Added the first actual feature of the app, the ability to create a project folder.
    Also added a mods folder because I'm planning on having mod support for the app.
    Also added a config file that holds some values for the app.
    I might change the file format of the app so that it won't be as easy to mess with.

--- Creator Hub Console.c
+++ Creator Hub Console.c
@@ -2,16 +2,23 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
+#include <sys/stat.h>
 
 void authorise(void);
+void createProject(void);
+void createModsFolder(void);
+void writeToConfig(void);
+void readConfig(void);
 
 const char username[]="admin";
 const int pass=123456;
 //username and password are temporary
 
-bool auth = false;
+bool auth;
+bool moded;
 
 int main(void){
+	readConfig();
 	char userInput[100]={0};
 	while (strcmp(userInput,".exit")!=0){
 		printf("%s\n",auth?"Admin mode":"User mode");
@@ -28,6 +35,7 @@ int main(void){
 					printf(".help: shows available commands\n");
 					printf(".clear: clears the console\n");
 					printf(".authorise: enter username and password to access more features\n");
+					printf(".project-n: create new project folder\n");
 					printf(".exit: exit the program\n");
 				}
 				if (strcmp(userInput,".clear")==0){
@@ -36,20 +44,38 @@ int main(void){
 				if (strcmp(userInput,".authorise")==0){
 					authorise();
 				}
+				if (strcmp(userInput,".project-n")==0){
+					createProject();
+				}
 				break;
 			}
 			case '!':{
 				if (auth==false){
 					printf("Please use .authorise to access feature\n");
 				} else {
-					
+					if (strcmp(userInput,"!help")==0){
+						printf(".help: shows available commands\n");
+						printf("!help: shows all commands\n");
+						printf(".clear: clears the console\n");
+						printf(".authorise: enter username and password to access more features\n");
+						printf(".project-n: create new project folder\n");
+						printf("!mods-enable: enables modding and creates a mods folder\n");
+						printf(".exit: exit the program\n");
+					}
+					if (strcmp(userInput,"!mods-enable")==0){
+						createModsFolder();
+					}	
 				}
+				break;
+			}
+			default:{
+				printf("Invalid command\n");
 				break;
 			}
 		}
 		
 	}
-	
+	writeToConfig();
 	return 0;
 }
 
@@ -69,5 +95,52 @@ void authorise(void){
 		auth=true;
 	} else {
 		printf("Incorrect username or password\n");
+	}
+}
+
+void createProject(void){
+	char folderName[100];
+    printf("Enter the project name: ");
+    scanf("%s", folderName);
+    if (mkdir(folderName) == 0) {
+        printf("Project '%s' created successfully.\n", folderName);
+    } else {
+        printf("Project to create folder '%s'.\n", folderName);
+    }
+}
+
+void createModsFolder(void){
+	if (moded){
+		printf("Mods folder already exists\n");
+		return;
+	}
+    if (mkdir("mods") == 0) {
+        printf("Mods folder created successfully.\n");
+        moded=true;
+    } else {
+        printf("Failed to create mods folder.\n");
+    }
+}
+
+void writeToConfig(void){
+	FILE *fptr = fopen("config.txt","w");
+	fprintf(fptr,"auth:%d\nmoded:%d",auth?1:0,moded?1:0);
+	fclose(fptr);
+}
+
+void readConfig(void){
+	int temp_auth,temp_mod;
+	FILE *fptr = fopen("config.txt","r");
+	fscanf(fptr,"auth:%d\nmoded:%d",&temp_auth,&temp_mod);
+	fclose(fptr);
+	if (temp_auth==1){
+		auth=true;
+	} else {
+		auth=false;
+	}
+	if (temp_mod==1){
+		moded=true;
+	} else {
+		moded=false;
 	}
 }
