d5a47d5c613d1c0cc70c96aba6ec6ca2aff21c09
theokarvoun <155096086+theokarvoun@users.noreply.github.com>
2024-03-08 08:25:14 +0200

    Added a few warnings to the setup.bat file

--- Commandlne_Version/setup.bat
+++ Commandlne_Version/setup.bat
@@ -1,2 +1,6 @@
 @echo off
-setx PATH "%PATH%;path\to\chub.exe"
+echo This will make the chub command accessible through the commandline
+echo This will be done by adding it to your systems environment variables
+echo If you don't want that close this window
+pause
+setx PATH "%PATH%;C:\path\to\chub.exe"
