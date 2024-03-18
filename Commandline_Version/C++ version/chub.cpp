e2e147329d7a46970b2a2d3eecef819f286ef698
theokarvoun <155096086+theokarvoun@users.noreply.github.com>
2024-03-18 16:15:06 +0200

    Attempting to make the code faster

--- Commandline_Version/C++ version/chub.cpp
+++ Commandline_Version/C++ version/chub.cpp
@@ -7,12 +7,58 @@
 int createProject(char *str);
 void createSubFolder(char *name);
 
-int main(int argc,char **argv){
-    if (argv[1] == NULL){
+const std::string init = "-init";
+
+int main(int argc, std::string argv[]){
+    if (argc < 1){
         std::cerr << args_err << std::endl;
         return 1;
     }
-    if (strcmp(argv[1],"-init")==0){
+    switch (argv[1]){
+        case init:{
+            if (argc < 2){
+            std::cerr << args_err << std::endl;
+            return 1;
+        }
+        switch (createProject(argv[2])){
+            case 0:{
+                std::cout << "Project Created Successfully" << std::endl;
+                return 0;
+            }
+            case 1:{
+                std::cerr << "An error occured during project creation" << std::endl;
+                return 1;
+            }
+        }
+        }
+        case ("-help" || "-h") :{
+            using namespace std;
+            cout << "-h or -help: shows commands" << endl;
+            cout << "-version: displays the current version" << endl;
+            cout << "-init <project name>: creates a new project folder in the current directory" << endl;
+            cout << "-console: launches the console version of the app" << endl;
+            return 0;
+        }
+        case "-version":{
+            using namespace std;
+            cout << "chub version alpha-0.0.2-C++" << endl;
+            cout << "Made by theokarvoun" << endl;
+            cout << "Check for newer versions on: https://github.com/theokarvoun/Creator-Hub-Console/tree/main/Commandlne_Version" << endl;
+            return 0;
+        }
+        case "-console":{
+            system("Creator-Hub-Console.exe");
+            return 0;
+        }
+        case "-shell":{
+            system("powershell");
+            return 0;
+        }
+        default:{
+            std::cerr << "Invalid Command\n Use -help" << std::endl;
+            return 0;
+        }
+    /*if (strcmp(argv[1],"-init")==0){
         if (argv[2]==NULL){
             std::cerr << args_err << std::endl;
             return 1;
@@ -52,32 +98,31 @@ int main(int argc,char **argv){
         return 0;
     }
     return 0;
+    */
+}
 }
 
-int createProject(char *str){
+int createProject(std::string str){
     if (!mkdir(str)) {
-        char *temp = (char*)malloc(50*sizeof(char));
+        std::string temp;
         strcpy(temp,str);
         strcat(temp,"\\footage");
         createSubFolder(temp);
-        free(temp);
-        temp = (char*)malloc(50*sizeof(char));
+        temp.clear();
+        temp.copy(str);
         strcpy(temp,str);
         strcat(temp,"\\sfx");
         createSubFolder(temp);
-        free(temp);
-        temp = (char*)malloc(50*sizeof(char));
         strcpy(temp,str);
         strcat(temp,"\\clips");
         createSubFolder(temp);
-        free(temp);
         return 0;
     } else {
         return 1;
     }
 }
 
-void createSubFolder(char *name){
+void createSubFolder(std::string name){
     if (mkdir(name)){
         std::cerr << "Couldn't create subfolder" << std::endl;
         return;
